a74f912911cbae68754f877fb273d016de97537e
ryanwalk01 <ryan.walk01@gmail.com>
2024-05-13 16:08:40 -0600

    W05 Learning Activity

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -1,9 +1,26 @@
 using System;
+using System.Drawing;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+
+        Square square = new Square("Red", 4);
+        shapes.Add(square);
+
+        Rectangle rectangle = new Rectangle("Green", 5, 6);
+        shapes.Add(rectangle);
+
+        Circle circle = new Circle("Blue", 7);
+        shapes.Add(circle);
+
+        foreach (Shape shape in shapes) {
+            string color = shape.GetColor();
+            double area = shape.GetArea();
+
+            Console.WriteLine($"Color:{color} | Area:{area}");
+        }
     }
 }
